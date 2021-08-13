56bb8571b841ef56078f820db3c8493b6afcc8fb
nicolas377 <programnicolas@gmail.com>
2021-08-12 20:19:54 -0400

    keep working on classes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 "use strict";
+function getRandomDoorNumber() {
+    return Math.floor(Math.random() * 3);
+}
 class door {
     info;
     constructor(info) {
@@ -7,7 +10,7 @@ class door {
     open() {
         this.info.isOpen = true;
     }
-    setPrize() {
+    addPrize() {
         this.info.hasPrize = true;
     }
 }
@@ -16,6 +19,7 @@ class stage {
     door1;
     door2;
     constructor() {
+        // init everything
         this.door0 = new door({
             number: 0,
             isOpen: false,
@@ -31,5 +35,32 @@ class stage {
             isOpen: false,
             hasPrize: false
         });
+        // put a prize behind a door
+        this[`door${getRandomDoorNumber()}`].addPrize();
+    }
+    getDoors() {
+        var toReturn = [];
+        toReturn.push(this.door0);
+        toReturn.push(this.door1);
+        toReturn.push(this.door2);
+        return toReturn;
+    }
+    openDoorWithoutPrize() {
+        var doors = this.getDoors();
+        // weed out the door with a prize
+        doors.forEach((element, index) => {
+            if (!element.info.hasPrize) {
+                doors = doors.splice(index, 1);
+            }
+        });
+        // open a random door without a prize
+        const randomIndex = Math.floor(Math.random() * doors.length);
+        doors[randomIndex].open();
+        return doors[randomIndex];
+    }
+    openDoor(doorNumber) {
+        this[`door${doorNumber}`].open();
+        return this[`door${doorNumber}`];
     }
 }
+var Stage = new stage();
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ interface DoorInfo {
   hasPrize: boolean;
 }
 
+function getRandomDoorNumber(): DoorNumber {
+  return Math.floor(Math.random() * 3) as DoorNumber
+}
+
 class door {
   info: DoorInfo
 
@@ -17,7 +21,7 @@ class door {
     this.info.isOpen = true
   }
 
-  setPrize() {
+  addPrize() {
     this.info.hasPrize = true
   }
 }
@@ -27,6 +31,7 @@ class stage {
   door1: door
   door2: door
   constructor() {
+    // init everything
     this.door0 = new door({
       number: 0,
       isOpen: false,
@@ -42,5 +47,44 @@ class stage {
       isOpen: false,
       hasPrize: false
     })
+    
+    // put a prize behind a door
+    this[`door${getRandomDoorNumber()}`].addPrize()
+  }
+
+  getDoors(): door[] {
+    var toReturn: door[] = []
+    toReturn.push(this.door0)
+    toReturn.push(this.door1)
+    toReturn.push(this.door2)
+    return toReturn
+  }
+
+  openDoorWithoutPrize(): door {
+    var doors = this.getDoors()
+
+    // weed out the door with a prize
+    doors.forEach((element, index) => {
+      if (!element.info.hasPrize) {
+        doors = doors.splice(index, 1)
+      }
+    })
+
+    // open a random door without a prize
+    const randomIndex: DoorNumber = Math.floor(Math.random()*doors.length) as DoorNumber
+    doors[randomIndex].open()
+
+    return doors[randomIndex]
+  }
+
+  openDoor(doorNumber: DoorNumber): door {
+    this[`door${doorNumber}`].open()
+    return this[`door${doorNumber}`]
   }
 }
+
+class player {
+  constructor() {}
+}
+
+var Stage = new stage()
